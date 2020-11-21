8f5a492be0080d13bb4c85455ae0e8f32fe085c0
田中杏直 <anji.tnk.140114@gmail.com>
2020-11-21 22:47:44 +0900

    export default

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -1,8 +1,10 @@
 import $ from 'jquery'
-import axios from 'axios'
-import { csrfToken } from 'rails-ujs'
+import axios from 'modules/axios'
+import {
+  listenInactiveHeartEvent,
+  listenActiveHeartEvent
+} from 'modules/handle_heart'
 
-axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
 
 const handleHeartDisplay = (hasLiked) => {
   if (hasLiked){
@@ -36,6 +38,9 @@ document.addEventListener('DOMContentLoaded', () => {
         appendNewComment(comment)
       })
     })
+    .catch((error) => {
+      window.alert('失敗')
+    })
 
     handleCommentForm()
 
@@ -62,31 +67,6 @@ document.addEventListener('DOMContentLoaded', () => {
     handleHeartDisplay(hasLiked)
     })
 
-  $('.inactive-heart').on('click', () => {
-    axios.post(`/articles/${articleId}/like`)
-     .then((response) => {
-       if (response.data.status === 'ok') {
-        $('.active-heart').removeClass('hidden')
-        $('.inactive-heart').addClass('hidden')
-       }
-     })
-     .catch((e) => {
-       window.alert('Error')
-       console.log(e)
-     })
+    listenInactiveHeartEvent(articleId)
+    listenActiveHeartEvent(articleId)
   })
-  $('.active-heart').on('click', () => {
-    axios.delete(`/articles/${articleId}/like`)
-     .then((response) => {
-      if (response.data.status === 'ok') {
-        $('.active-heart').addClass('hidden')
-        $('.inactive-heart').removeClass('hidden')
-       }
-     })
-     .catch((e) => {
-       window.alert('Error')
-       console.log(e)
-     })
-  })
-})
-
